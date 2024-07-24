44379fe273b9b97b32fe251d8e12f8b29f0b718f
aurelkysabudiman <aurelkysabudiman@gmail.com>
2024-07-24 14:39:44 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                                             href="{{ route('register') }}"
                                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            Register
+                                            Register YOO
                                         </a>
                                     @endif
                                 @endauth
@@ -163,6 +163,7 @@
                     </main>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                        <a href="About">About</a>
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
                 </div>
